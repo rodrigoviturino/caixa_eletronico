2ca852e0cb3d8e44f4df4a15ebfcae32b35f2328
Rodrigo Viturino <viturino_souza@outlook.com>
2020-01-22 16:20:57 -0300

    Criação do arquivo add-transacao, para ser feita a logica de movimentação de valores

--- index.php
+++ index.php
@@ -50,5 +50,47 @@ if(isset($_SESSION['banco']) && !empty($_SESSION['banco']) ) {
     </p>
 
     <a href="sair.php">Sair</a>
+    <hr>
+
+    <h3>Movimentação / Extrato</h3>
+
+    <a href="add-transacao.php">Adicionar Valor</a>
+
+    <table border="1" width="600">
+        <thead>
+            <tr>
+                <th>Data</th>
+                <th>Valor</th>
+            </tr>
+        </thead>
+        <tbody>
+            <?php
+                $sql = $pdo->prepare(" SELECT * FROM historico WHERE id_conta = :id_conta");
+                $sql->bindValue(":id_conta", $id);
+                $sql->execute();
+
+                if($sql->rowCount() > 0 ){
+                    $info = $sql->fetchAll();
+
+                    foreach ($info as $item) {
+                ?>
+                    <tr>
+                        <td><?= date('d/m/y H:i', strtotime($item['data_operacao'])); ?></td>
+                        <td>
+                            <?php if($item['tipo'] == 0 ): ?>
+                                <span color="green"> R$: + <?= $item['valor'] ?> </span>
+                            <?php else: ?>
+                                <span color="red"> R$: - <?= $item['valor'] ?> </span>
+                            <?php endif; ?>
+                        </td>
+                    </tr>
+                <?php 
+                    }
+                }
+            ?>
+        </tbody>
+    </table>
+
+
 </body>
 </html>
